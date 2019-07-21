33148d2c2b658cda703daa173ea9829713d0599e
Choon How <choonhow@gmail.com>
2019-07-21 10:51:16 +0800

    update script

--- install.sh
+++ install.sh
@@ -2,16 +2,10 @@
 
 sudo apt-get update
 
-whoami
-
 sudo apt-get --yes --force-yes install openjdk-8-jdk
 
 mkdir /etc/cloudx
 
-ls /etc/cloudx
-
-wget https://github.com/choonhow/AzureTest/raw/master/target/sample-web-0.1.0.jar -O /etc/cloudx
-
-ls /etc/cloudx
+wget https://github.com/choonhow/AzureTest/raw/master/target/sample-web-0.1.0.jar -O /etc/cloudx/sample-web-0.1.0.jar
 
 nohup java -jar /etc/cloudx/sample-web-0.1.0.jar > /etc/cloudx/nohup.out 2>&1&
