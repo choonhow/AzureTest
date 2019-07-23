295ad6db6d303880bad2c7b3802c7c4dcc54ac46
Choon How <choonhow@gmail.com>
2019-07-23 17:52:01 +0800

    change from java to static html

--- install.sh
+++ install.sh
@@ -2,14 +2,20 @@
 
 #sudo apt-get update
 #sudo apt-get --yes --force-yes install openjdk-8-jdk
-sudo yum -y install java-1.8.0-openjdk
+#sudo yum -y install java-1.8.0-openjdk
 
-sudo firewall-cmd --add-port=8080/tcp --permanent
+sudo yum -y install httpd
+
+sudo service start
+
+sudo firewall-cmd --add-port=80/tcp --permanent
 
 sudo systemctl restart firewalld
 
-mkdir /etc/cloudx
+rm -rf /var/www/html/*
 
-wget https://github.com/choonhow/AzureTest/raw/master/target/sample-web-0.1.0.jar -O /etc/cloudx/sample-web-0.1.0.jar
+wget https://github.com/choonhow/AzureTest/raw/master/static/pud.zip -O /var/www/html/pud.zip
 
-nohup java -jar /etc/cloudx/sample-web-0.1.0.jar > /etc/cloudx/nohup.out 2>&1&
+unzip /var/www/html/pud.zip -d /var/www/html/
+
+mv  /var/www/html/pud/* /var/www/html/.
